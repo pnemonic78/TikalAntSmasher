1411124fc0246c9455433fc21c8f79e466c8571e
Moshe <moshe.waisberg@tikalk.com>
2017-11-16 15:54:01 +0200

    model

---
+++ app/src/main/java/com/tikalk/antsmasher/model/AntLocation.java
@@ -0,0 +1,18 @@
+package com.tikalk.antsmasher.model;
+
+/**
+ * Ant location.
+ */
+
+public class AntLocation {
+
+    public final float x;
+    public final float y;
+    public final String id;
+
+    public AntLocation(float x, float y, String id) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/tikalk/antsmasher/model/AntSmash.java
@@ -0,0 +1,16 @@
+package com.tikalk.antsmasher.model;
+
+/**
+ * Ant smash.
+ */
+
+public class AntSmash {
+
+    public final String id;
+    public final long timestamp;
+
+    public AntSmash(String id, long timestamp) {
+        this.id = id;
+        this.timestamp = timestamp;
+    }
+}
